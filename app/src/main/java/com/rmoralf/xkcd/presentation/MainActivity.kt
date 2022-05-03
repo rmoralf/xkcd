b2b35699d298bc7196be840a5190bb47191572a3
Rodrigo <rmoralf@gmail.com>
2022-05-03 17:40:50 +0200

    Add Firebase Analytics and Crashlytics

--- app/src/main/java/com/rmoralf/xkcd/presentation/MainActivity.kt
+++ app/src/main/java/com/rmoralf/xkcd/presentation/MainActivity.kt
@@ -3,14 +3,20 @@ package com.rmoralf.xkcd.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.rmoralf.xkcd.presentation.comic.ComicScreen
 import com.rmoralf.xkcd.presentation.ui.theme.XkcdTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        firebaseAnalytics = Firebase.analytics
         setContent {
             XkcdTheme {
                 ComicScreen()
